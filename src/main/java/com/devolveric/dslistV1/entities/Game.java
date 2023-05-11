5afae1a5040758a33cf1c47a725cfc4b9aff5546
olvericc <olv.ericlopes@gmail.com>
2023-05-11 01:52:21 -0300

    game seed in h2 database

--- src/main/java/com/devolveric/dslistV1/entities/Game.java
+++ src/main/java/com/devolveric/dslistV1/entities/Game.java
@@ -15,9 +15,12 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
@@ -25,13 +28,14 @@ public class Game {
 
     public Game(Long id, String title,
                 Integer year, String genre,
-                String platform, String imgUrl,
+                String platforms, Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -69,12 +73,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
