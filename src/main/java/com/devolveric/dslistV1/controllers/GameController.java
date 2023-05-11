2ba94606bed1043233691aaf53b1ef2c658b46b7
eric.oliveira <olv.ericlopes@gmail.com>
2023-05-11 10:20:11 -0300

    improving a return method in game controller

--- src/main/java/com/devolveric/dslistV1/controllers/GameController.java
+++ src/main/java/com/devolveric/dslistV1/controllers/GameController.java
@@ -17,8 +17,7 @@ public class GameController {
     private GameService gameService;
     @GetMapping
     public List<GameMinDTO> findAll(){
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 
 }
